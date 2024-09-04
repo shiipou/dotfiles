16d5b04b68aba279a62ccae1a8f4a9410563f769
shiipou <shiishii@nocturlab.fr>
2024-09-04 08:27:19 +0200

    feat!: Initial commit with nvim config and .bashrc

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,27 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+
+vim.wo.relativenumber = true
